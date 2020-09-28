13f0e442a52d539bce8ab94a3036f0411b76b3c4
VGSTTEST <71207922+VGSTTEST@users.noreply.github.com>
2020-09-29 00:36:21 +0530

    Update login.js

--- TESTVGST/Annexure-III(A)/js/login.js
+++ TESTVGST/Annexure-III(A)/js/login.js
@@ -1,4 +1,3 @@
-// Your web app's Firebase configuration
 var firebaseConfig = {
     apiKey: "AIzaSyBGO2B6hQTK4K5OE5zrbJ02vjdi6f28Erk",
     authDomain: "vgstproposals.firebaseapp.com",
@@ -9,100 +8,80 @@ var firebaseConfig = {
     appId: "1:552421108644:web:6e33c08cbc16366cdeef31",
     measurementId: "G-0QNPLQVH6Y"
   };
-// Initialize Firebase
   firebase.initializeApp(firebaseConfig);
   firebase.analytics();
 
     
-    const auth = firebase.auth();
-	var provider = new firebase.auth.GoogleAuthProvider();
-var user;
-function google(){
-  firebase.auth().signInWithPopup(provider).then(function(result) {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    var token = result.credential.accessToken;
-    // The signed-in user info.
-    user = result.user;
-    console.log(user);
-    var messagesRef = firebase.database().ref(user.uid+'/users'); 
-    var newPrincipalInvestigator = messagesRef.push();
-    newPrincipalInvestigator.set(
-        {
-        Name:user.name,
-   
-        }
-    );
-    // ...
-  }).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-    // ...
-  });
-}
-	
-	function signUp(){
-		
-		var email = document.getElementById("email");
-		var password = document.getElementById("password");
-		
-		const promise = auth.createUserWithEmailAndPassword(email.value, password.value);
-		promise.catch(e => alert(e.message));
-		
-		alert("Signed Up");
-	}
-	
-	
-	
-	function signIn(){
-		
-		var email = document.getElementById("email");
-		var password = document.getElementById("password");
-		
-		const promise = auth.signInWithEmailAndPassword(email.value, password.value);
-		promise.catch(e => alert(e.message));
-		
-		
-		
-		
-	}
-	
-	
-	function signOut(){
-		
-		auth.signOut();
-		alert("Signed Out");
-		
-	}
-	
-	
-	
-	auth.onAuthStateChanged(function(user){
-		
-		if(user){
-			
-			var email = user.email;
-			alert("Active User " + email);
-			
-			//Take user to a different or home page
+        const auth 		= firebase.auth();
+		const temail 	= document.getElementById('temail');
+		const tpassword = document.getElementById('tpassword');
+		const signUp 	= document.getElementById('signUp');
+		const login 	= document.getElementById('login');
+		const email 	= temail.value;
+		const password 	= tpassword.value;
 
-			//is signed in
+
+	signUp.addEventListener('click',e =>{
+		const email 	= temail.value;
+		const password 	= tpassword.value;
+		const auth 		= firebase.auth();
+		const promise 	= auth.createUserWithEmailAndPassword(email,password);
+		promise.catch(e => console.log(e.message));
+		alert("Signed Up");
+	});
 			
-		}else{
-			
-			alert("No Active User");
-			//no user is signed in
-		}
-		
-		
+	login.addEventListener('click',e =>{
+		const email 	= temail.value;
+		const password 	= tpassword.value;
+        const auth 		= firebase.auth();
+		const promise 	= auth.signInWithEmailAndPassword(email,password);
+		promise.catch(e =>alert(e.message));
+		alert("Signed in");
+	});
+
+
+	logout.addEventListener('click',e =>{		
+		firebase.auth().signOut();
+		alert("Signed Out");
 		
 	});
 
 
+	firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+		var errorCode 	 = error.code;
+		var errorMessage = error.message;
+	  });
+
+	  firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+		var errorCode    = error.code;
+		var errorMessage = error.message;
+	  });
+	  firebase.auth().createUserWithEmailAndPassword(email, password)
+				.catch(function(error) {
+				var errorCode = error.code;
+				var errorMessage = error.message;
+				if (errorCode == 'auth/weak-password') {
+					alert('The password is too weak.');
+				} else {
+					alert(errorMessage);
+				}
+				console.log(error);
+		});
+	
+	firebase.auth().onAuthStateChanged(firebaseUser => {
+		if(firebaseUser){
+			
+			var email = firebaseUser.email;
+			alert("Active User " + email);
+			console.log(firebaseUser);
+			
+			
+		}else{
+			
+			alert("No Active User");
+		}
+	});
+		
 	const signInWithGoogleButton = document.getElementById('signInWithGoogle');
 
 	const signInWithGoogle = () => {
@@ -119,5 +98,34 @@ function google(){
 	  
 	  signInWithGoogleButton.addEventListener('click', signInWithGoogle);
 
-	
+	  function myFunction() {
+		document.getElementById("login").style.display = "none";
+		document.getElementById("signUp").style.display = "block";
+		document.getElementById("verifypassword").style.display = "block";
+		document.getElementById("reg").style.display = "none";
+		document.getElementById("start2").style.display = "block";
+		document.getElementById("start1").style.display = "none";
+		document.getElementById("temail").style.display = "block";
+		document.getElementById("tpassword").style.display = "block";
+		document.getElementById("signUp").style.display = "none";
+		document.getElementById("next").style.display = "block";
+		document.getElementById("otp").style.display = "none";
+	  }
+	  function refresh(){
+		document.getElementById("login").style.display = "block";
+		document.getElementById("signUp").style.display = "none";
+		document.getElementById("verifypassword").style.display = "none";
+		document.getElementById("reg").style.display = "inline";
+		document.getElementById("start2").style.display = "none";
+		document.getElementById("start1").style.display = "block";
 
+	  }
+
+	  function enterotp(){
+		document.getElementById("verifypassword").style.display = "none";
+		document.getElementById("temail").style.display = "none";
+		document.getElementById("tpassword").style.display = "none";
+		document.getElementById("otp").style.display = "block";
+		document.getElementById("next").style.display = "none";
+		document.getElementById("login").style.display = "block";
+	  }
